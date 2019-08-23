f04e0a5e55c165aca7fa54f5880a8a694bb96d25
Calvin McDowell <calvinmac40@hotmail.com>
2019-08-23 00:24:42 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,7 +2,11 @@ require 'pry'
 
 def my_find(collection)
   i = 0 
-  while i < collection.length 
+  while i < collection.length
+  if 
+  yield(collection[i])
+  return collection([i])
+end
   i += 1 
   end
 end
